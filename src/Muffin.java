3dc0e028a10b2d456a33a7dfd852833ac72ef57f
Faizan Dhankwala <faizandhankwala1@gmail.com>
2025-10-22 18:33:47 -0500

    initial commit

---
+++ src/Muffin.java
@@ -0,0 +1,38 @@
+public class Muffin {
+   private int price;
+    private Boolean blueberry;
+
+    public Muffin(boolean blueberry) {
+
+        this.blueberry = blueberry;
+        if (this.blueberry) {
+            this.price = 400;
+        } else {
+            this.price = 300;
+        }
+    }
+    private int baseprice;
+
+
+    public void addToppings(){
+
+    }
+    public void addCream(){
+
+    }
+    public void addSoda(){
+
+    }
+    public void toGo(){
+
+    }
+
+    }
+
+
+
+
+
+
+
+
